f70bca0143970282c06878dba4f0482097717079
Gihan_Sachith <gihansachith92@gmail.com>
2024-01-21 22:20:23 +0530

    make deposite function

---
+++ slot_machine/main.py
@@ -0,0 +1,15 @@
+
+def deposite():
+  while True:
+    amount = input("what would you like to deposite? $")
+    if amount.isdigit():
+      amount = int(amount)
+      if amount > 0:
+        break
+      else:
+        print("Amount must be greater than 0.")
+
+    else:
+      print("Please enter a number.")
+
+  return amount         
